7298023a55465c3721b332da207b62143c2ea724
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-06-10 10:51:47 +0500

    types add IDeal

--- query/use-status-query.ts
+++ query/use-status-query.ts
@@ -1,14 +1,34 @@
 import { useQuery } from '@tanstack/vue-query';
 import { Query } from 'appwrite';
-import { COLLECTION_DEALS, DB_ID } from '~/constants';
+import { COLLECTION_DEALS, DB_ID, status } from '~/constants';
 import { DATABASE } from '~/libs/appwrite';
 import { useAuthStore } from '~/store/auth.store';
+import type { IColumn, IDeal } from '~/types';
 export const useStatusQuery = () => {
     const { currentUser } = useAuthStore();
     return useQuery({
         queryKey: ['deals'],
         queryFn: () =>
             DATABASE.listDocuments(DB_ID, COLLECTION_DEALS, [Query.equal('userId', currentUser.id)]),
-        select: (data) => console.log(data),
+        select: (data) => {
+            const newBoard: IColumn[] = status.map(item => ({
+                ...item,
+                items: []
+            }))
+            const deals = data.documents as unknown as IDeal[]
+            for (const deal of deals) {
+                const column = newBoard.find(item => item.id === deal.status)
+                if (column) {
+                    column.items.push({
+                        $createdAt: deal.$createdAt,
+                        name: deal.name,
+                        description: deal.description,
+                        status: deal.status,
+                        $id: deal.$id,
+                    })
+
+                }
+            }
+        },
     })
 }
